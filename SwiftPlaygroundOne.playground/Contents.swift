3fff41b1425dd76564486ac88174b3da26551d2e
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-08-30 14:43:53 -1000

    “Completed Swift Playgrounds #1”

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
 var greeting = "Hello, playground"
+var myFirstVariable: String
+
+
+myFirstVariable = "I’ve created my first variable!"
+
+var mySecondVariable = " And look, I added 2 strings together!"
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
+
+var myFourthVariable = "One, two, buckle my shoe. "
+
+var myFifthVariable = "Three, four, buckle some more. "
+
+var mySixthVariable = "Five, six, Nike kicks."
+
+var mySeventhVariable = myFourthVariable + myFifthVariable + myFifthVariable
+
+print(mySeventhVariable)
